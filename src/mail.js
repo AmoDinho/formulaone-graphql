8ea2e663d025d545fc533ff5ad99fb5fab520b3f
amodinho <u14284783@tuks.co.za>
2018-11-05 18:21:31 +0200

    Trying to get the mail reset to send a message.

--- src/mail.js
+++ src/mail.js
@@ -1,16 +1,13 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer')
+const postmarkTransport = require('nodemailer-postmark-transport')
 
-const transport = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    auth:{
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-    },
-});
+const transport = nodemailer.createTransport(postmarkTransport({
+	auth: {
+		apiKey: '2bf8257c-c345-4f43-b553-611a60c7b789'
+	}
+}))
 
-
-const mail = text => `
+const mailTemp = text => `
   <div className="email" style="
   border: 1px solid black;
   padding: 20px;
@@ -19,11 +16,10 @@ const mail = text => `
   font-size: 20px;
   ">
   <h2>Hello dear friend!</h2>
-  <p>${text}</p>
-
+  <p>${text}</p> 
   <p>From the team at Fan Boost</p>
   </div>
-  `;
+  `
 
-  exports.transport = transport;
-  exports.mail = mail;
+exports.transport = transport
+exports.mailTemp = mailTemp
