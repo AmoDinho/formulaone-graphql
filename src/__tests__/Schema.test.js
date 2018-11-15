938ceb7fade0fb346e97d711ecb6096af8c91617
amodinho <u14284783@tuks.co.za>
2018-11-15 14:19:05 +0200

    Trying to get schema test to work.

--- src/__tests__/Schema.test.js
+++ src/__tests__/Schema.test.js
@@ -1,8 +1,6 @@
-import {
-	makeExecutableSchema,
-	addMockFunctionsToSchema
-} from 'graphql-tools'
+import {makeExecutableSchema,addMockFunctionsToSchema} from 'graphql-tools'
 import {graphql} from 'graphql'
+import {find,filter} from 'lodash';
 
 const typeDefs = `
   type Driver {
@@ -18,21 +16,31 @@ const typeDefs = `
     championshipWins:Int!
     postedBy:User
     boosts: [FanBoost!]! @relation(name:"FanBoost",onDelete:CASCADE)
-}
-`
 
-const schema = makeExecutableSchema({typeDefs})
+    type Query {
+        drivers: [Driver]
+        driver(id:String!): Driver
+    }
+
+}`
+
+const resolvers = {
+    Query: {
+        drivers: () => drivers,
+        driver: (_, {id}) => find(drivers,{id:id})
+
+    },
+    Driver: {
+        drivers: driver => filter(drivers, {driverId: driver.id})
+    }
+}
+
+const schema = makeExecutableSchema({typeDefs, resolvers})
 
 addMockFunctionsToSchema({schema})
 
-const query = `
- query driverQuery{
+const drivers = [
+    {id:'1',createdAt:'2018-09-09',number:2,name:'Giggs',team:'Grimey',points:3,pictureURL:'png.url.cdn',country: 'Britian',podiums: 3,championshipWins:2,postedBy: 'Skepta',boosts:2}
+]
 
-     driver{
-     id
-     name
-     }
- }
-`
-
-graphql(schema,query).then((results) => console.log('Got Result', results))
+graphql(schema).then((results) => console.log('Got Result', results))
