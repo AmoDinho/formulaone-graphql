77cd6357e87850f9cf169394ffaac643de024b4e
amodinho <u14284783@tuks.co.za>
2018-12-06 18:18:52 +0200

    Making sure the server can detect when it is prod/dev to read the correct endpoint

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const server = new GraphQLServer({
         ...req,
         db: new Prisma({
             typeDefs: 'src/generated/prisma.graphql',
-            endpoint: 'https://fan-boost-new-eb982e7d8c.herokuapp.com/boost/dev/',
+            endpoint:  process.env.NODE_ENV==="development" ? process.env.PRISMA_ENDPOINT : 'https://fan-boost-new-eb982e7d8c.herokuapp.com/boost/dev/',
             secret: process.env.PRISMA_SECRET,
             debug: process.env.NODE_ENV==="development" ? true:false,
         }),
